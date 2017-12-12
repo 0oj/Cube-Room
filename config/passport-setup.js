547d2fedb1b37c41e3c944ab79ccb7ec51ab056c
OojAmit <oojamit@gmail.com>
2017-12-12 20:09:54 +0530

    Save an retrive users to and from mongoDB(mLab)

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,10 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../models/user')
+
+passport.serializeUser((user, done) => done(null, user.id))
+passport.deserializeUser((id, done) => User.findOneById(id).then(user => done(null, user.id)))
 
 passport.use(
   new GoogleStrategy({
@@ -8,6 +12,17 @@ passport.use(
     clientID: keys.google.clientID,
     clientSecret: keys.google.clientSecret
   }, (accessToken, refreshToken, profile, done) => {
-    console.log(profile);
+    User.findOne({googleId: profile.id}).then((currentUser) => {
+      if (currentUser) {
+        console.log('User already in the DB');
+        done(null, currentUser)
+      } else {
+        new User({
+          username: profile.displayName,
+          googleId: profile.id
+        }).save().then(newUser => done(null, currentUser))
+      }
+
+    })
   })
 )
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 const port = process.env.PORT || 10003;
 
@@ -8,6 +10,8 @@ var app = express();
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 app.use('/auth', require('./routes/auth'))
+
+mongoose.connect(keys.mongodb.dbURI, () => console.log('Conected to MongoDB'))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html')
